5de1e34bfa44debaba9dfbdf3b3c0c07ae19ec16
vich3er bc2024-4 <cherednikviktoriya@gmail.com>
2024-10-23 21:15:58 +0300

    get

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const http = require('http');
-const { Command } =  require('commander');
-const fs =  require('fs').promises;
+const { Command } = require('commander');
+const fs = require('fs').promises;
+const path = require('path');
 const program = new Command();
 
 program
     .requiredOption('-h, --host <host>', 'Host address')
     .requiredOption('-p, --port <port>', 'Port number')
-    .requiredOption('-c, --cache <cache>', 'cache directory');
+    .requiredOption('-c, --cache <cache>', 'Cache directory');
 
 program.parse(process.argv);
-const options =  program.opts();
+const options = program.opts();
 const host = options.host;
-const port =  options.port;
-const cache =  options.cache;
+const port = options.port;
+const cache = options.cache;
 
-const server =  http.createServer(async(req, res) => {
-    res.end('server is running')
+const getCachedImagePath = (statusCode) => path.join(cache, `${statusCode}.jpg`);
+
+const server = http.createServer(async (req, res) => {
+    const statusCode = req.url.slice(1);
+    const cachedImagePath = getCachedImagePath(statusCode);
+
+    if (req.method === 'GET') {
+        try {
+
+            const data = await fs.readFile(cachedImagePath);
+            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            res.end(data);
+        } catch (err) {
+
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('404 Not Found');
+            } else {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            }
+        }
+    }
+
+
+
 });
 
 server.listen(port, host, () => {
-    console.log(`Сервер запущено на http://${host}:${port}/`)
+    console.log(`Сервер запущено на http://${host}:${port}/`);
 });
